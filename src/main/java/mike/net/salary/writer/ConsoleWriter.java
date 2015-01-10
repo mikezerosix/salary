598877132f3fc2ad658891e19b87982249b6a423
Michael Holopainen <xregistration@gmail.com>
2015-01-10 03:35:45 +0200

    Done

--- src/main/java/mike/net/salary/writer/ConsoleWriter.java
+++ src/main/java/mike/net/salary/writer/ConsoleWriter.java
@@ -5,12 +5,15 @@ import mike.net.salary.model.Salary;
 public class ConsoleWriter {
 
     public void writeSalary(Salary salary) {
-        System.out.printf("%d %s, Total: $%,.2f \n", salary.getId(), salary.getName(), salary.getTotal());
-        System.out.printf("\t  Base salary   : $%,.2f \n",salary.getBaseSalary());
-        System.out.printf("\t  Evening hours : $%,.2f \n",salary.getIrregularHoursSalary());
-        System.out.printf("\t  Overtime 25%% : $%,.2f \n",salary.getOvertimeQuarter());
-        System.out.printf("\t  Overtime 50%% : $%,.2f \n",salary.getOvertimeHalf());
-        System.out.printf("\t  Overtime 100%%: $%,.2f \n",salary.getOvertimeFull());
+        System.out.printf("%d, %s\n", salary.getId(), salary.getName());
+        System.out.printf("\t  Work hours   \t: $%,.2f \n",salary.getBaseSalary());
+        System.out.printf("\t  Evening hours\t: $%,.2f \n",salary.getIrregularHoursSalary());
+        System.out.printf("\t  Overtime 25%% \t: $%,.2f \n",salary.getOvertimeQuarter());
+        System.out.printf("\t  Overtime 50%% \t: $%,.2f \n",salary.getOvertimeHalf());
+        System.out.printf("\t  Overtime 100%%\t: $%,.2f \n",salary.getOvertimeFull());
+        System.out.printf("\t  -----------------------\n");
+        System.out.printf("\t          Total: $%,.2f  \n",  salary.getTotal());
+        System.out.printf("\t          ===============\n\n");
     }
 
 }
